7da2462d3cd4afe1535d83f386fc1b5c0af8f815
jagtapshweta <147432748+jagtapshweta@users.noreply.github.com>
2024-11-13 09:11:13 +0530

    Add files via upload

---
+++ pr1/frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure it checks all files where Tailwind classes might be used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
